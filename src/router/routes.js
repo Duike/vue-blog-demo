951b257e520b26a3df8ed4f6f48373419da1c85a
shizl <szl@123456>
2019-07-12 14:42:12 +0800

    vue+vue-router+elementUi

---
+++ src/router/routes.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from "../views/Login";
+
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/login',
+      component: Login,
+      name: 'login'
+    },
+
+    {
+      path: '/',
+      component: Login,
+      name: 'login'
+    },
+
+  ]
+
+
+})
