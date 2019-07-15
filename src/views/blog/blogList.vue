4e25e234e3ed39eb2918aa32cb9e1f1cb1e4f96a
shizl <szl@123456>
2019-07-15 18:11:28 +0800

    1.优化token验证方式。

--- src/views/blog/blogList.vue
+++ src/views/blog/blogList.vue
@@ -17,31 +17,21 @@
           //获取部门信息
         getDepartmentList(){
           let _this = this;
+          //请求参数
+          let params = {
+
+          };
           this.$axios({
             url: 'xqd/department/findTemplateList',
-            method: 'get',//请求的方式
+            method: 'post',//请求的方式
             headers: {
               'Content-Type': 'application/json; charset=UTF-8',
-
+              'token':sessionStorage.getItem("Token")
             },
-            data:JSON.stringify({})
+            data:JSON.stringify(params)
           }).then(res => {
             console.log(res);
-            if (res.data.status == "异常报错"){
-              this.$message({
-                message: res.data.message,
-                type: "warning"
-              });
 
-              // let _this = this;
-              // setTimeout(function(){
-              //   _this.$router.push({
-              //     path: "/login"
-              //   });
-              // },2000)
-
-
-            }
 
           }).catch(err => {
             console.log('报错的信息', err);
